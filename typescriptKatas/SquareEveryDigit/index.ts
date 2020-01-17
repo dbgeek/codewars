272efb051345c7b9d8517abfa257368d54cdc08e
Björn Ahl <bnal@ba78.me>
2020-01-17 18:25:28 +0100

    kata: square every digit

---
+++ typescriptKatas/SquareEveryDigit/index.ts
@@ -0,0 +1,8 @@
+export class Kata {
+    static squareDigits(num: number) {
+      const c = num.toString().split('').map( val => parseInt(val) ** 2);
+      c.join
+    }
+  }
+
+console.log(Kata.squareDigits(9119), 811181);
