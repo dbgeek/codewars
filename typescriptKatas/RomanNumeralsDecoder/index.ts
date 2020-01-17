9cf82dcc44bcd8a86f8c1daa54670ee82ed06e3d
Björn Ahl <bnal@ba78.me>
2020-01-17 18:26:32 +0100

    kata: refactor roman number al decoder

--- typescriptKatas/RomanNumeralsDecoder/index.ts
+++ typescriptKatas/RomanNumeralsDecoder/index.ts
@@ -1,5 +1,16 @@
 export function solution(roman: string): number {
-
+    const RomanNumber: Record<string, number> = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    return roman.split('').reduce( (sum, value,idx, array)  => RomanNumber[array[idx + 1]] > RomanNumber[value] ? sum - RomanNumber[value] : sum + RomanNumber[value], 0)
 }
 
 console.log(solution('MMVIII'), 2008);
+console.log(solution('IV'), 4);
+console.log(solution('IX'), 9);
