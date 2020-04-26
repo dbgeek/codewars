0d0c223198a74b4926d23c33657b16c9d9f15fc1
Björn Ahl <bnal@ba78.me>
2020-04-26 18:24:33 +0200

    feature: typescript kata Yourorderplease

---
+++ typescriptKatas/src/Yourorderplease/order.test.ts
@@ -0,0 +1,11 @@
+import { order } from './order';
+
+test('test', () => {
+    expect(order("is2 Thi1s T4est 3a")).toEqual("Thi1s is2 3a T4est");
+});
+test('test', () => {
+    expect(order("4of Fo1r pe6ople g3ood th5e the2")).toEqual("Fo1r the2 g3ood 4of th5e pe6ople");
+});
+test('test', () => {
+    expect(order("")).toEqual("");
+});
